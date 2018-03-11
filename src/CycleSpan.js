12ada367fc7f6c2303261a05f98ad2248eb2083b
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2018-03-11 14:42:03 -0700

    Added rendering tests

--- src/CycleSpan.js
+++ src/CycleSpan.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const CycleSpan = (props) => {
-	return <span></span>; 
+	return <span className='cycle'>{ preprocessText(props.text) }</span>; 
+}
+
+const splitOnUnescapedWhitespace = (rawText) => {
+	if (rawText == null)
+		return [];
+	// Match character sequences, except for special whitespace characters,
+	// and match those separately.
+	const re = /(\\t|\\n|\\r)|[^\\]+/g;
+	return rawText.match(re);
+}
+
+const preprocessText = (rawText) => {
+	
+	const splitText = splitOnUnescapedWhitespace(rawText);
+	let sectionKey = 0;
+	return splitText.map((text) => {
+		// TODO: Is there a better way to render tabs?
+		if (text.includes('\\t')) {
+			return <span key={ 'tab' + sectionKey }>&#9;&#9;&#9;&#9;</span>;
+		} else if (text.includes('\\r')) {
+			return <br key={ ++sectionKey } />;
+		} else if (text.includes('\\n')) {
+			return <br key={ ++sectionKey } />;
+		}
+		return <span key={ 'text' + sectionKey }>{ text }</span>;
+	});
+
 }
 
 export default CycleSpan;
--- src/CycleSpan.test.js
+++ src/CycleSpan.test.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import CycleSpan from './CycleSpan';
 
-it('renders without crashing', () => {
-	const div = document.createElement('div');
-	ReactDOM.render(<CycleSpan />, div);
-	ReactDOM.unmountComponentAtNode(div);
+describe('CycleSpan - Smoke', () => {
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<CycleSpan />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
 });
+
+describe('CycleSpan - Render', () => {
+
+	let renderer;
+
+	beforeEach(() => {
+		renderer = new ShallowRenderer();
+	});
+
+	afterEach(() => {
+		renderer.unmount();
+	});
+
+	it('renders a span', () => {
+		renderer.render(<CycleSpan />);
+		const output = renderer.getRenderOutput();
+		expect(output.type).toBe('span');
+	});
+
+	it('renders text passed in with the correct section key', () => {
+		const text = 'test text';
+		renderer.render(<CycleSpan text={ text } />);
+		const output = renderer.getRenderOutput();
+
+		const outputChildren = output.props.children;
+		const sectionKey = '0';
+		const expectedChildren = [
+			<span key={ 'text' + sectionKey }>{ text }</span>
+		];
+
+		expect(outputChildren).toEqual(expectedChildren);
+		expect(JSON.stringify(outputChildren)).toEqual(
+			JSON.stringify(expectedChildren));
+	});
+
+	it('renders whitespace correctly', () => {
+
+		const firstParagraph = 'This is a paragraph.';
+		const firstSectionKey = '0';
+		const secondParagraph = 'Another paragraph - wow ';
+		const secondSectionKey = '1';
+
+		const text = '\t' + firstParagraph + '\n\t' + secondParagraph;
+		renderer.render(<CycleSpan text={ text } />);
+		const output = renderer.getRenderOutput();
+		expect(output.props.children).toEqual([
+			<span key={ 'text' + firstSectionKey }>{ text }</span>
+		]);
+
+	});
+
+});
+
