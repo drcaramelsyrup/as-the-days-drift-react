a9f857301cf32bb3d4409123a31418c9173699d4
drcaramelsyrup <dr.caramel.syrup@gmail.com>
2018-03-29 08:53:44 -0700

    App tests, move deep transform to python entweedle instead?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CycleSpan from './CycleSpan';
+import Passage from './Passage';
+
+/* Wishlist */
+// - ImmutableJS
+// - Redux (if necessary)
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <CycleSpan text="\tThis is a paragraph\n\tAnother paragraph\nNo tabs here" />
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: props.data
+		};
+	}
+
+	loadDataFromFile(file) {
+		this.setState({
+			data: JSON.parse(file)
+		});
+	}
+
+	setData(data) {
+		this.setState({ data: data });
+	}
+	
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Welcome to React</h1>
+				</header>
+				<p className="App-intro">
+					To get started, edit <code>src/App.js</code> and save to reload.
+				</p>
+				<Passage data={ this.state.data } />
+			</div>
+		);
+	}
 }
+
+// const unicodifyWhitespace = (data) => {
+// 	// if array, check if string, replace whitespace
+// 	// if string, replace whitespace
+
+// 	const unicodified = (text) => {
+// 		const replaceTabs = text.replace('\t', '\u0009');
+// 		const replaceNewlines = replaceTabs.replace('\n', '\u000a');
+// 		return replaceNewlines.replace('\r', '\u000a');
+// 	};
+
+
+
+// 	const deepWalk = (data) => {
+// 		// if (data is array)
+// 		const keys = Object.keys(data);
+
+// 		if (keys.length === 1)
+
+
+// 		return keys.map((key) => {
+// 			return deepWalk(data[key]);
+// 		});
+// 	}
+// 	return Object.keys(data).map((key) => {
+
+// 	});
+
+
+// }
+
+// const deepTransform = (data, transformation) => {
+
+// 	if (Array.isArray(data)) {
+// 		return data.reduce((datum) => {
+// 			return deepTransform(datum, transformation);
+// 		});
+// 	}
+
+// 	if (typeof(data) === 'string') {
+// 		return transformation(data);
+// 	}
+
+// 	const keys = Object.keys(data);
+
+// 	return keys.map((key) => {
+// 		return deepTransform(data[key], transformation);
+// 	});
+// }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import App from './App';
+import Passage from './Passage';
+import data from './test_passage.json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App - Smoke', () => {
+
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+
+});
+
+describe('App - Render', () => {
+
+	let renderer;
+
+	beforeEach(() => {
+		renderer = new ShallowRenderer();
+	});
+
+	afterEach(() => {
+		renderer.unmount();
+	});
+
+	it('renders as a div', () => {
+		renderer.render(<App />);
+		const output = renderer.getRenderOutput();
+		expect(output.type).toBe('div');
+	});
+
+	it('contains a Passage element', () => {
+		renderer.render(<App />);
+		const output = renderer.getRenderOutput();
+		expect(output.props.children).toContainEqual(<Passage />);
+	});
+
+	it('correctly sets data passed into the Passage element', () => {
+		const testPassage = {
+			pid: 0,
+			text: 'test text'
+		};
+		renderer.render(<App data={ testPassage } />);
+		const output = renderer.getRenderOutput();
+		expect(output.props.children).toContainEqual(<Passage data={ testPassage } />);
+	});
+
+	// it('correctly converts whitespace to Unicode escape characters', () => {
+	// 	const testText = 'test \ttext\n \t \r text';
+	// 	const testTextUnicode = 'test \u0009text\u000a \u0009 \u000a text';
+	// 	const testPassage = { pid: 0, text: testText };
+	// 	const testPassageUnicode = { pid: 0, text: testTextUnicode };
+	// 	renderer.render(<App data={ testPassage } />);
+	// 	const output = renderer.getRenderOutput();
+	// 	expect(output.props.children).toContainEqual(
+	// 		<Passage data={ testPassageUnicode } />);
+		
+	// });
+
+
 });
